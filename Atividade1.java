870bc895597c8e9f0bed165ce71ff03d422389c0
Guilhermedev798 <guilhermeabreu3212@gmail.com>
2026-01-03 18:19:12 -0300

    Atualizar o Atividade1.java

--- Atividade1.java
+++ Atividade1.java
@@ -6,12 +6,11 @@ public class Atividade1 {
 
         System.out.print("Digite a temperatura em Celsius: ");
         double celsius = sc.nextDouble();
-
-        // Conversão das temperaturas
+
         double fahrenheit = celsius * 9.0 / 5.0 + 32.0;
         double kelvin = celsius + 273.15;
 
-        // Exibição dos resultados formatados
+ formatados
         System.out.println("\n-- Conversão de Temperaturas --");
         System.out.printf("Temperatura em Celsius: %.2f°C%n", celsius);
         System.out.printf("Temperatura em Fahrenheit: %.2f°F%n", fahrenheit);
