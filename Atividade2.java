c685ca557217eab1c92584a28845767b5e118bb4
Guilhermedev798 <guilhermeabreu3212@gmail.com>
2026-01-03 18:20:13 -0300

    Atualizar o Atividade2.java

--- Atividade2.java
+++ Atividade2.java
@@ -5,26 +5,24 @@ public class Atividade2 {
         Scanner sc = new Scanner(System.in);
         double[] notas = new double[8];
 
-        // Entrada das notas
         for (int i = 0; i < 8; i++) {
             System.out.print("Digite a nota " + (i + 1) + ": ");
             notas[i] = sc.nextDouble();
         }
-
-        // Cálculo das médias bimestrais
+
         double bimestre1 = (notas[0] + notas[1]) / 2.0;
         double bimestre2 = (notas[2] + notas[3]) / 2.0;
         double bimestre3 = (notas[4] + notas[5]) / 2.0;
         double bimestre4 = (notas[6] + notas[7]) / 2.0;
 
-        // Cálculo das médias semestrais
+        
         double semestre1 = (bimestre1 + bimestre2) / 2.0;
         double semestre2 = (bimestre3 + bimestre4) / 2.0;
 
-        // Cálculo da média final anual
+        
         double mediaFinal = (semestre1 + semestre2) / 2.0;
 
-        // Impressão dos resultados formatados
+         formatados
         System.out.println("\n--- Resultados ---");
         System.out.printf("1º Bimestre: %.2f%n", bimestre1);
         System.out.printf("2º Bimestre: %.2f%n", bimestre2);
